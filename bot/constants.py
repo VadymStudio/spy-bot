b5985737906825e7d22a81110bf1bf40e329b86e
VadymStudio <vadym.martseniuk2007@gmail.com>
2025-11-18 11:27:58 +0200

    blaaaa

--- bot/constants.py
+++
@@ -1,7 +0,0 @@
-# bot/constants.py
-import os
-import logging
-
-logger = logging.getLogger(__name__)
-
-DB_PATH = os.getenv('RENDER_DISK_PATH', '') + '/players.db' if os.getenv('RENDER_DISK_PATH') else 'players.db'
